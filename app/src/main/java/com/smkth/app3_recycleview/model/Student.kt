55cacd11b609a70193373145a547e4cbbdb98a79
elsyee <sukmaelsye@gmail.com>
2025-08-06 08:52:10 +0700

    add edit n hapus

--- app/src/main/java/com/smkth/app3_recycleview/model/Student.kt
+++ app/src/main/java/com/smkth/app3_recycleview/model/Student.kt
@@ -1,7 +1,7 @@
 package com.smkth.app3_recycleview.model
 
 data class Student(
-    val nama: String,
-    val nis: String,
-    val kelas: String
+    var nama: String,
+    var nis: String,
+    var kelas: String
 )
